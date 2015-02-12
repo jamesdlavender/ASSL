9cb421c9b3a8e6a8831a77b5104d57c8f49db551
James Lavender <me@devrea.com>
2015-02-12 00:11:24 -0500

    Structured files for delivery.
    Structured files for delivery.

---
+++ chat.ty_Week2/application/views/template/menu.php
@@ -0,0 +1,22 @@
+<div class="header">
+<a href="http://localhost/chat.ty/index.php/user/registration" style=" float:right; text-decoration:none; margin-right:110px; color:#FFFFFF; font-weight:bold;">Sign up</a>
+<a href="http://localhost/chat.ty/main/" style=" float:right; text-decoration:none; margin-right:10px; color:#FFFFFF; font-weight:bold;">Login</a>
+
+
+
+</div>
+
+<style>
+body{
+width:80%;
+height:75%;
+border:1px solid #000000;
+}
+.header{
+width:100%;
+height:35px;
+margin-left:0px;
+background: #990000;
+text-decoration:none;
+}
+</style>
