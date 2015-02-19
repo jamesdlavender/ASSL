0a0b200454c4b9d99212e90b3deb35ddf88c30a9
James Lavender <me@jamesdlavender.com>
2015-02-18 19:01:15 -0800

    Added Week 3
    Added Week 3 and updated main branch.

--- chat.ty/application/views/restricted.php
+++
@@ -1,2 +0,0 @@
-<h3>You must login to access this page.</h3><br />
-<a href='<?php echo base_url()."main/login" ?>'>Back to login</a>
--- chat.ty/application/views/v_content.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$this->load->view('template/header');
-$this->load->view($content);
-//$this->load->view('template/right');
-$this->load->view('template/footer');
-?>
